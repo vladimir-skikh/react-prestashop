03ebfb0de296cc63a179dbece63bb1fc998ac011
vladimir-skikh <19vladimir99@gmail.com>
2021-04-02 14:12:54 +0300

    working module

--- pworderhistories/controllers/admin/adminorderhistories.php
+++ pworderhistories/controllers/admin/adminorderhistories.php
@@ -13,6 +13,10 @@ class AdminOrderHistoriesController extends ModuleAdminController
             'static_folder' => _PS_BASE_URL_SSL_.'/modules/pworderhistories/build/static/'
         ));
 
+        Media::addJsDef(array(
+            'buildURL' => _PS_BASE_URL_SSL_.'/modules/pworderhistories/build/',
+        ));
+
         $this->content .= $this->context->smarty->fetch(_PS_MODULE_DIR_.'/pworderhistories/build/index.tpl');
 
         $this->context->smarty->assign(array(
